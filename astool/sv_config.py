a589e82d674d7bcf7507ce081bf73d31413d1f2c
karin <summertriangle.dev@gmail.com>
2020-08-25 22:17:13 -0700

    EN 1.7.1 config

--- astool/sv_config.py
+++ astool/sv_config.py
@@ -299,6 +299,22 @@ Unj9vj6kOBlOGv4JWQIDAQAB
 
 SERVER_CONFIG["en"] = [
     {
+        "root": "https://gl-real-prod-8f2jln5l4evlw5l1.api.game25.klabgames.net/ep1074",
+        # @astool_OSS_REDACT_START
+        "bootstrap_key": "dzL8pF9JNfSkswgH",
+        "session_mixkey": [
+            "31F1F9DC7AC4392D1DE26ACF99D970E425B63335B461E720C73D6914020D6014",
+            "78D53D9E645A0305602174E06B98D81F638EAF4A84DB19C756866FDDAC360C96",
+        ],
+        # @astool_OSS_REDACT_END
+        "public_key": PUBLIC_KEY_DEFAULT_EN,
+        "user_agent": "global/7346 CFNetwork/1107.1 Darwin/19.0.0",
+        "master_keys": [0x5f4adcfb, 0x785673c4, 0x4b77a214],
+        "bundle_version": "1.7.1",
+        "language": "en",
+        "additional_languages": ["ko", "th", "zh"],
+    },
+    {
         "root": "https://gl-real-prod-8f2jln5l4evlw5l1.api.game25.klabgames.net/ep1073",
         # @astool_OSS_REDACT_START
         "bootstrap_key": "dzL8pF9JNfSkswgH",
