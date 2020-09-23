b4f3c2df8e1c545c131f6bfb96e44b123b22258a
karin <summertriangle.dev@gmail.com>
2020-09-23 14:31:23 -0700

    jp 2.0.0

--- astool/sv_config.py
+++ astool/sv_config.py
@@ -62,6 +62,20 @@ SERVER_CONFIG = {}
 
 SERVER_CONFIG["jp"] = [
     {
+        "root": "https://jp-real-prod-v4tadlicuqeeumke.api.game25.klabgames.net/ep2000",
+        # @astool_OSS_REDACT_START
+        "bootstrap_key": "GzP2hBe26jJ0wVD8",
+        "session_mixkey": [
+            "31F1F9DC7AC4392D1DE26ACF99D970E425B63335B461E720C73D6914020D6014",
+            "78D53D9E645A0305602174E06B98D81F638EAF4A84DB19C756866FDDAC360C96",
+        ],
+        # @astool_OSS_REDACT_END
+        "public_key": PUBLIC_KEY_DEFAULT_JP,
+        "user_agent": "allstars/37572 CFNetwork/1197 Darwin/20.0.0",
+        "master_keys": [0x6b1e3fef, 0x5bb97a20, 0x553650cf],
+        "bundle_version": "2.0.0",
+    },
+    {
         "root": "https://jp-real-prod-v4tadlicuqeeumke.api.game25.klabgames.net/ep1081",
         # @astool_OSS_REDACT_START
         "bootstrap_key": "J86GSiQghiHEbDjD",
