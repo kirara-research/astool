5d2a69de85cd633e78beff86736ed8d6be9e5297
karin <summertriangle.dev@gmail.com>
2020-12-06 17:09:23 -0800

    EN 2.0.0 keys
    Note: we have extended the version number to 4 digits,
    and will be bumping the last digit on key updates from now on.

--- astool/sv_config.py
+++ astool/sv_config.py
@@ -354,6 +354,22 @@ Unj9vj6kOBlOGv4JWQIDAQAB
 # @astool_OSS_REDACT_END
 SERVER_CONFIG["en"] = [
     {
+        "root": "https://gl-real-prod-8f2jln5l4evlw5l1.api.game25.klabgames.net/ep2003",
+        # @astool_OSS_REDACT_START
+        "bootstrap_key": "ufzSibMqdY9r2vDH",
+        "session_mixkey": [
+            "31F1F9DC7AC4392D1DE26ACF99D970E425B63335B461E720C73D6914020D6014",
+            "78D53D9E645A0305602174E06B98D81F638EAF4A84DB19C756866FDDAC360C96",
+        ],
+        # @astool_OSS_REDACT_END
+        "public_key": PUBLIC_KEY_DEFAULT_EN,
+        "user_agent": "global/7346 CFNetwork/1107.1 Darwin/19.0.0",
+        "master_keys": [0x1da66214, 0x0c067a5f, 0x11177a3c],
+        "bundle_version": "2.0.0",
+        "language": "en",
+        "additional_languages": ["ko", "th", "zh"],
+    },
+    {
         "root": "https://gl-real-prod-8f2jln5l4evlw5l1.api.game25.klabgames.net/ep1083",
         # @astool_OSS_REDACT_START
         "bootstrap_key": "vvmX7f8kdcakDuPm",
