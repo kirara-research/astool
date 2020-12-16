02a7112639a3313a946b6c973a261489eb181bc8
karin <summertriangle.dev@gmail.com>
2020-12-16 01:35:29 -0800

    version bump: 1.0.1.x

--- setup.py
+++ setup.py
@@ -10,8 +10,8 @@ def main():
         packages=["astool", "astool_extra"],
         ext_modules=[],
         package_data={"astool": ["ex_bootstrap_script/*.json"]},
-        install_requires=["cryptography<3.0", "requests<3.0.0", "plac<2.0.0"],
-        extras_require={"async_pkg": ["aiohttp<3.7.0"]}
+        install_requires=["cryptography<4.0.0", "requests<3.0.0", "plac<2.0.0"],
+        extras_require={"async_pkg": ["aiohttp<4.0.0"]}
     )
 
     setup(**args)
