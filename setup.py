a91b2fb88e49065e0a7d70818619b3e7e214391a
karin <summertriangle.dev@gmail.com>
2021-04-20 19:51:24 -0700

    en 2.2.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, Extension
 def main():
     args = dict(
         name="astool",
-        version="1.1.0.10",
+        version="1.1.0.11",
         description="Tooling for interacting with remote and local AS resources.",
         packages=["astool", "astool_extra"],
         ext_modules=[],
