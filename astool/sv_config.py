0ca2eceb2ea264c68f69d08c7b18c3f497a29916
karin <summertriangle.dev@gmail.com>
2021-05-10 19:46:35 -0700

    en 2.3.0

--- astool/sv_config.py
+++ astool/sv_config.py
@@ -424,6 +424,22 @@ Unj9vj6kOBlOGv4JWQIDAQAB
 # @astool_OSS_REDACT_END
 SERVER_CONFIG["en"] = [
     {
+        "root": "https://gl-real-prod-8f2jln5l4evlw5l1.api.game25.klabgames.net/ep2033",
+        # @astool_OSS_REDACT_START
+        "bootstrap_key": "4BObXqQTPJjzl8dL",
+        "session_mixkey": [
+            "31F1F9DC7AC4392D1DE26ACF99D970E425B63335B461E720C73D6914020D6014",
+            "78D53D9E645A0305602174E06B98D81F638EAF4A84DB19C756866FDDAC360C96",
+        ],
+        # @astool_OSS_REDACT_END
+        "public_key": PUBLIC_KEY_DEFAULT_EN,
+        "user_agent": "global/7346 CFNetwork/1107.1 Darwin/19.0.0",
+        "master_keys": [0x237b0834, 0x39514119, 0x40b00cc3],
+        "bundle_version": "2.3.0",
+        "language": "en",
+        "additional_languages": ["ko", "th", "zh"],
+    },
+    {
         "root": "https://gl-real-prod-8f2jln5l4evlw5l1.api.game25.klabgames.net/ep2024",
         # @astool_OSS_REDACT_START
         "bootstrap_key": "qKzO8ddLIc6f14GQ",
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, Extension
 def main():
     args = dict(
         name="astool",
-        version="1.1.0.11",
+        version="1.1.0.12",
         description="Tooling for interacting with remote and local AS resources.",
         packages=["astool", "astool_extra"],
         ext_modules=[],
