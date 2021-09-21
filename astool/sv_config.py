3bd6d59db384d79498fac511dff6cfca0162d267
k <summertriangle.dev@gmail.com>
2021-09-20 23:20:54 -0700

    3.0.0

--- astool/sv_config.py
+++ astool/sv_config.py
@@ -62,6 +62,20 @@ SERVER_CONFIG = {}
 
 SERVER_CONFIG["jp"] = [
     {
+        "root": "https://jp-real-prod-v4tadlicuqeeumke.api.game25.klabgames.net/ep3000",
+        # @astool_OSS_REDACT_START
+        "bootstrap_key": "bna6dWRwUBecVLXo",
+        "session_mixkey": [
+            "31F1F9DC7AC4392D1DE26ACF99D970E425B63335B461E720C73D6914020D6014",
+            "78D53D9E645A0305602174E06B98D81F638EAF4A84DB19C756866FDDAC360C96",
+        ],
+        # @astool_OSS_REDACT_END
+        "public_key": PUBLIC_KEY_DEFAULT_JP,
+        "user_agent": "allstars/37572 CFNetwork/1197 Darwin/20.0.0",
+        "master_keys": [0x44ede0ab, 0x0fd9af9d, 0x0c4235f6],
+        "bundle_version": "3.0.0",
+    },
+    {
         "root": "https://jp-real-prod-v4tadlicuqeeumke.api.game25.klabgames.net/ep2050",
         # @astool_OSS_REDACT_START
         "bootstrap_key": "ZHS0y952mVsJ1TFb",
@@ -466,6 +480,22 @@ Unj9vj6kOBlOGv4JWQIDAQAB
 """
 # @astool_OSS_REDACT_END
 SERVER_CONFIG["en"] = [
+    {
+        "root": "https://gl-real-prod-8f2jln5l4evlw5l1.api.game25.klabgames.net/ep3000",
+        # @astool_OSS_REDACT_START
+        "bootstrap_key": "w5f3NGCIS8CWahTu",
+        "session_mixkey": [
+            "31F1F9DC7AC4392D1DE26ACF99D970E425B63335B461E720C73D6914020D6014",
+            "78D53D9E645A0305602174E06B98D81F638EAF4A84DB19C756866FDDAC360C96",
+        ],
+        # @astool_OSS_REDACT_END
+        "public_key": PUBLIC_KEY_DEFAULT_EN,
+        "user_agent": "global/7346 CFNetwork/1107.1 Darwin/19.0.0",
+        "master_keys": [0x44ede0ab, 0x0fd9af9d, 0x0c4235f6],
+        "bundle_version": "3.0.0",
+        "language": "en",
+        "additional_languages": ["ko", "th", "zh"],
+    },
     {
         "root": "https://gl-real-prod-8f2jln5l4evlw5l1.api.game25.klabgames.net/ep2050",
         # @astool_OSS_REDACT_START
