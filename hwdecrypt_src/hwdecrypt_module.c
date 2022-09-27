6044d16c6b18f30640aabef7b6631b2253ef8c95
t <summertriangle.dev@gmail.com>
2022-09-27 03:14:42 -0700

    hwdecrypt: support multi-phase (PEP 489) init, subinterpreters, and fastcall

--- hwdecrypt_src/hwdecrypt_module.c
+++ hwdecrypt_src/hwdecrypt_module.c
@@ -16,39 +16,52 @@ typedef struct {
     struct hwd_keyset pk;
 } hwd_keyset_t;
 
+int hwdecrypt_exec_module(PyObject *module);
 static int keyset_init(hwd_keyset_t *self, PyObject *args, PyObject *kwds);
-static PyObject *decrypt_buffer(PyObject *self, PyObject *args);
+static PyObject *decrypt_buffer(PyObject *self, PyObject *const *args, Py_ssize_t nargs);
 
-static PyMemberDef HWDKeysetTypeMembers[] = {
+static const PyMemberDef HWDKeysetTypeMembers[] = {
     {"key1", T_UINT, offsetof(hwd_keyset_t, pk.k1), 0, "key1"},
     {"key2", T_UINT, offsetof(hwd_keyset_t, pk.k2), 0, "key2"},
     {"key3", T_UINT, offsetof(hwd_keyset_t, pk.k3), 0, "key3"},
     {NULL} /* Sentinel */
 };
-
-static PyTypeObject HWDKeysetType = {
-    PyVarObject_HEAD_INIT(NULL, 0)
-    .tp_name = "hwdecrypt.Keyset",
-    .tp_doc = "Contains the RNG state for the stream cipher.",
-    .tp_basicsize = sizeof(hwd_keyset_t),
-    .tp_itemsize = 0,
-    .tp_flags = Py_TPFLAGS_DEFAULT,
-    .tp_new = PyType_GenericNew,
-    .tp_init = (initproc)keyset_init,
-    .tp_members = HWDKeysetTypeMembers,
+static const PyType_Slot HWDKeysetTypeSlots[] = {
+    {Py_tp_doc, "Contains the RNG state for the stream cipher."},
+    {Py_tp_new, PyType_GenericNew},
+    {Py_tp_init, (initproc)keyset_init},
+    {Py_tp_members, &HWDKeysetTypeMembers},
+    {0, NULL}
+};
+static const PyType_Spec HWDKeysetType = {
+    .name = "hwdecrypt.Keyset",
+    .basicsize = sizeof(hwd_keyset_t),
+    .itemsize = 0,
+    .flags = Py_TPFLAGS_DEFAULT | Py_TPFLAGS_HEAPTYPE,
+    .slots = &HWDKeysetTypeSlots,
 };
 
-static PyMethodDef HWDTopLevel[] = {
-    {"decrypt", decrypt_buffer, METH_VARARGS, "Decrypt the data within a buffer object."},
+static const PyMethodDef HWDTopLevel[] = {
+    {"decrypt", decrypt_buffer, METH_FASTCALL, "Decrypt the data within a buffer object."},
     {NULL, NULL, 0, NULL}
 };
+
+static const PyModuleDef_Slot modslots[] = {
+    {Py_mod_exec, &hwdecrypt_exec_module},
+    {0, NULL}
+};
+
+typedef struct {
+    PyObject *keyset_type;
+} hwd_module_private_t;
 
 static PyModuleDef modinfo = {
     PyModuleDef_HEAD_INIT,
     .m_name = "hwdecrypt",
     .m_doc = "C extension module for decrypting ICE data.",
-    .m_size = -1,
+    .m_size = sizeof(hwd_module_private_t),
     .m_methods = HWDTopLevel,
+    .m_slots = &modslots,
 };
 
 #define hwd_K3_DEFAULT 0x3039
@@ -70,10 +83,23 @@ static int keyset_init(hwd_keyset_t *self, PyObject *args, PyObject *kwds) {
     return 0;
 }
 
-static PyObject *decrypt_buffer(PyObject *self, PyObject *args) {
+static PyObject *decrypt_buffer(PyObject *self, PyObject *const *args, Py_ssize_t nargs) {
+    hwd_module_private_t *private = PyModule_GetState(self);
     hwd_keyset_t *keyset;
     Py_buffer edata;
-    if (!PyArg_ParseTuple(args, "O!w*", &HWDKeysetType, &keyset, &edata)) {
+
+    if (nargs != 2) {
+        PyErr_SetString(PyExc_TypeError, "Wrong number of arguments.");
+        return NULL;
+    }
+
+    if (Py_TYPE(args[0]) != private->keyset_type) {
+        PyErr_SetString(PyExc_TypeError, "The first argument must be a Keyset.");
+        return NULL;
+    }
+    keyset = (hwd_keyset_t *)args[0];
+
+    if (PyObject_GetBuffer(args[1], &edata, PyBUF_WRITABLE | PyBUF_SIMPLE) != 0) {
         return NULL;
     }
 
@@ -89,19 +115,21 @@ static PyObject *decrypt_buffer(PyObject *self, PyObject *args) {
     Py_RETURN_NONE;
 }
 
+int hwdecrypt_exec_module(PyObject *module) {
+    PyObject *kstype_real = PyType_FromSpec(&HWDKeysetType);
+    if (PyModule_AddObject(module, "Keyset", (PyObject *)kstype_real) < 0) {
+        Py_DECREF(kstype_real);
+        return -1;
+    }
+
+    hwd_module_private_t *private = PyModule_GetState(module);
+    private->keyset_type = kstype_real;
+    return 0;
+}
+
 PyMODINIT_FUNC PyInit_hwdecrypt(void) {
-    PyObject *m;
-    if (PyType_Ready(&HWDKeysetType) < 0)
-        return NULL;
-
-    m = PyModule_Create(&modinfo);
-    if (m == NULL)
-        return NULL;
-
-    Py_INCREF(&HWDKeysetType);
-    if (PyModule_AddObject(m, "Keyset", (PyObject *)&HWDKeysetType) < 0) {
-        Py_DECREF(&HWDKeysetType);
-        Py_DECREF(m);
+    PyObject *m = PyModuleDef_Init(&modinfo);
+    if (m == NULL) {
         return NULL;
     }
 
